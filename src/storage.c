35dea36046cc6f05eb89e1b9dfa2f2c9ea56e8c0
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2026-02-23 14:58:54 +0100

    Risolto problema con cartelle
    Signed-off-by: Lorenzo Rocca <lorenzo.rocca.work@gmail.com>

--- src/storage.c
+++ src/storage.c
@@ -4,24 +4,31 @@
 
 #include "../includes/storage.h"
 
-#include <dirent.h>
 #include <errno.h>
 
 #ifdef _WIN32
     #include <direct.h>
+    #include <windows.h>
     #define MKDIR(path) _mkdir(path)
 #else
     #include <sys/stat.h>
-#define MKDIR(path) mkdir(path, 0755)
+    #include <sys/types.h>
+    #include <dirent.h>
+    #define MKDIR(path) mkdir(path, 0755)
 #endif
 
 void fix_data_folder() {
+#ifdef _WIN32
+    const DWORD ftyp = GetFileAttributesA("data");
+    if (ftyp == INVALID_FILE_ATTRIBUTES) {
+        MKDIR("data");
+    }
+#else
     DIR* dir = opendir("data");
     if (dir) {
         closedir(dir);
-    }
-    else if (ENOENT == errno) {
-        // Crea la cartella
+    } else if (errno == ENOENT) {
         MKDIR("data");
     }
+#endif
 }
